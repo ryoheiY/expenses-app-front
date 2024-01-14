e91a25174e69cc1a7cc638376f28a2f4d533f66b
ryode <ryo.dev.tech@outlook.com>
2024-01-14 23:54:13 +0900

    Keycloakによる認証処理を追加

--- src/commponents/Transportation/RecntlyApplication.js
+++ src/commponents/Transportation/RecntlyApplication.js
@@ -19,6 +19,8 @@ function RecentryApplication() {
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 2022-12-31
+                            </th><th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                2022-12-31
                             </th>
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 市ヶ尾
